a8bf009a8041eca31069d537a8b20a6d87e5641a
Kevin Marx <kevin.marx@tstmedia.com>
2012-12-09 21:18:59 -0600

    everything working properly now

--- jquery.overflowing.js
+++ jquery.overflowing.js
@@ -6,10 +6,10 @@
  */
 (function($){
   $.fn.overflowing = function(options, callback){
-    var self = this,
-        overflowed = [],
-        hasCallback = callback && typeof callback === 'function' ? true : false,
-        status = false
+    var self = this
+    var overflowed = []
+    var hasCallback = callback && typeof callback === 'function' ? true : false;
+    var status = false
     this.options = options || window
 
     this.each(function(){
@@ -22,17 +22,20 @@
       parents.push($parentsTo)
 
       for(var i=0; i<parents.length; i++){
+        var parentPosition = $(parents[i]).position()
         var parentWidth = $(parents[i]).width()
         var parentHeight = $(parents[i]).height()
-        if ( elPosition.top < 0 || elPosition.left < 0 || elPosition.top > parentHeight || elPosition.left > parentWidth || elPosition.top + elHeight > parentHeight || elPosition.left + elWidth > parentWidth) {
+        if ( elPosition.top<0
+        || elPosition.left<0
+        || elPosition.top>(parentHeight+parentPosition.top)
+        || elPosition.left>(parentWidth+parentPosition.left)
+        || (elPosition.top + elHeight) > (parentHeight+parentPosition.top)
+        || (elPosition.left+elWidth) > (parentWidth+parentPosition.left)){
           status = true
+          $(parents[i]).addClass('overflowed')
           overflowed.push(parents[i])
           if (hasCallback) callback(parents[i])
         }
-      }
-
-      for(var i=0; i<overflowed.length; i++){
-        $(overflowed[i]).addClass('overflowed')
       }
 
       if($this.parents(self.options).hasClass('overflowed')) $this.addClass('overflowing')
